ee20767694ef270db7dadbe2eab4ea1fd0a943fd
nitish2334 <raunaksinha1803@gmail.com>
2026-05-04 23:19:06 +0530

    assignment

--- jsDom/script.js
+++ jsDom/script.js
@@ -1,28 +1,30 @@
-function submit()
-{
-    console.log("submit button clicked");
+function Submit() {
+  console.log("Submit Button Clicked");
 
+  const fn = document.getElementById("fullName").value;
+  console.log(fn);
+  const city = document.getElementById("city").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const feedback = document.getElementById("feedback").value;
 
-    const fn = document.getElementById("fullname").value
-    console.log(fn);
-    const em = document.getElementById("email").value
-    console.log(em);
+  document.getElementById("myData").innerText = fn;
+  document.getElementById("fullName").value = "";
 
+  document.getElementById("myData4").innerText = city;
+  document.getElementById("city").value = "";
 
+  document.getElementById("myData2").innerText = email;
+  document.getElementById("email").value = "";
 
-    document.getElementById("my data").innerText = fn;
-    document.getElementById("my data").value = "";
-      document.getElementById("my email").innerText=em;
+  document.getElementById("myData3").innerText = password;
+  document.getElementById("password").value = "";
 
-    document.getElementById("my email").value = "";
+  
 
+  document.getElementById("myData5").innerText = feedback;
+  document.getElementById("feedback").value = "";
 
-
-
-
-      document.getElementById("datacard").classList.add("divshow");
-
-    document.getElementById("datacard").classList.remove("divhide");
-
+  document.getElementById("dataCard").classList.add("divShow");
+  document.getElementById("dataCard").classList.remove("divHide");
 }
-    
