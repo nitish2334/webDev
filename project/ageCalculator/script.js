2b7485987833d6d015cf5be3f2248aa3cf6952a8
nitish2334 <raunaksinha1803@gmail.com>
2026-05-12 18:59:59 +0530

    assisgnment

--- project/ageCalculator/script.js
+++ project/ageCalculator/script.js
@@ -1,40 +1,33 @@
-document.querySelector("form").addEventListener("submit", function(event) {
-    event.preventDefault();
-    
-    const dob = new Date(document.getElementById("DOB").value);
-    const currentDate = new Date(document.getElementById("currentDate").value);
+// document.querySelector("form").addEventListener("submit",(event) =>{
+//     event.defaultPrevented();
+//     const dob= document.getElementById("DOB").value; 
+//      const CurDate= document.getElementById("CurrentDate").value; 
 
-    const Age = Number(currentDate.split("-")[0]);
-
-
-
-
-
-
-
-
+//      const Age= Number( CurDate.split("-")[0]) - Number (dob.split("-")[0]);
+//      document.getElementById("DOB").value=""; 
+//      document.getElementById("CurrentDate").value="";
+//     //  console.log(dob);
+//     //  console.log(CurDate);
+//      document.getElementById("Age").innerText=`Your Age is : $ {age} Years`;
+     
+// })
 
 
 
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault();
 
+  const dob = document.getElementById("DOB").value;
+  const curDate = document.getElementById("CurrentDate").value;
 
+  //   let curYear = Number(curDate.split("-")[0]);
+  //   let dobYear = Number(dob.split("-")[0]);
+  //   const Age = curYear - dobYear;
 
+  const Age = Number(curDate.split("-")[0]) - Number(dob.split("-")[0]);
 
+  document.getElementById("DOB").value = "";
+  document.getElementById("CurrentDate").value = "";
 
-
-
-    console.log(dob);
-    console.log(currentDate);
-
-
-
-    
-    let age = currentDate.getFullYear() - dob.getFullYear();
-    const monthDiff = currentDate.getMonth() - dob.getMonth();
-    
-    if (monthDiff < 0 || (monthDiff === 0 && currentDate.getDate() < dob.getDate())) {
-        age--;
-    }
-    
-    document.getElementById("Myage").textContent = age;
+  document.getElementById("MyAge").innerText = Age;
 });
