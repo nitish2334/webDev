3a83c9c3dc256250f78a06235bd096f426162706
nitish2334 <raunaksinha1803@gmail.com>
2026-05-21 19:53:37 +0530

    Add multiple features including tip calculator, form validation, and password manager

---
+++ Form_validation/script.js
@@ -0,0 +1,26 @@
+function submit()
+{
+    
+    
+    const fn = document.getElementById("fullname").value;
+    const nm = document.getElementById("number").value;
+    const em = document.getElementById("email").value;
+   
+
+
+
+   o-o
+4L
+5
+    if(!/ ^[A-Za-z\s]+$/ .test(fn))
+    {
+        document.getElementById("nameError")="invalid full name"
+    }
+
+   
+    console.log(nm);
+
+    console.log(em);
+    
+    console.log(wd);
+}
